95afaaf5a8a126f39d2b62ad157e620cd940a31d
besomuk <srdjan.pavlica@gmail.com>
2018-09-16 09:59:17 +0200

    0.0.4
    Neke pogresno otkucane sitnice...

--- application/views/pages/comments_write.php
+++ application/views/pages/comments_write.php
@@ -2,9 +2,9 @@
 <h2>Novi komentar</h2>
 <p>
     <?php echo form_open('view_message/' . $id) ?>
-        
+
         Autor:<input type="text" name="author" value="Anonimus"><br>
-        Teekst:<textarea name="text" rows="10" cols="30"></textarea><br />
+        Tekst:<textarea name="text" rows="10" cols="30"></textarea><br />
         <input type="submit" name="submit" value="Posalji komentar" />
     </form>
 </p>
